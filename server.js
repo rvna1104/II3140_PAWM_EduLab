3d7c132f0fa35e8d6d9187ee8959e5614419b3c2
rvna1104 <limkaervina@gmail.com>
2024-11-15 18:48:27 +0700

    update

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const knex = require('knex');
 const jwt = require('jsonwebtoken');
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
 
 const db = knex({
   client: 'pg',
